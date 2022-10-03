ed2aa0e9169a803bd759ecec9e209c2280c54ba3
andy krassowski <akrassowski@gmail.com>
2022-10-02 20:11:03 -0400

    autopep8 cleanup

--- src/Shape.py
+++ src/Shape.py
@@ -28,11 +28,11 @@ COLOR_MAP = {
     'PURPLE': '#c03bff', 'BLUE': '#0632ff', 'RED': '#ff2600', 'GREEN': '#00fa00',
     'YELLOW': '#fffb00', 'CYAN': '#00fdff', 'MAGENTA': '#ff41ff', 'ORANGE': '#ff9500'
 }
-WIDE_EDGE_LINE_WIDTH, THIN_EDGE_LINE_WIDTH = 3, 1
 PI = 3.14159
-HATCH_MAP = {0:None, 1:None, 2: "--", 3: "||"}
+HATCH_MAP = {0: None, 1: None, 2: "--", 3: "||"}
 
 ZORDER_INC = 1
+
 
 class Shape():
     """holds shape attributes and helpers"""
@@ -41,10 +41,7 @@ class Shape():
 
     def __init__(self, args, which, data, info):
         """create flattened Shape attributes from DDS attributes"""
-        self.poly = None  # will be set by create
         self.which = which
-        self.angle = 0
-        self.fillKind = 0
         self.seq = info.reception_sequence_number.value
         self.zorder = ZORDER_INC
 
@@ -53,35 +50,23 @@ class Shape():
         self.scolor = data['color']  # keep sample color for debugging
         self.color = COLOR_MAP[self.scolor]
         if args.extended:
-            #self.angle = 0 if data['angle'] is None else data['angle']
             self.angle = data['angle']
-            self.fillKind = 0 if data['fillKind']is None else data['fillKind']
             self.fillKind = data['fillKind']
+        else:
+            self.angle = 0
+            self.fillKind = 0
         LOG.debug(f'created {self=}')
 
     def get_sequence_number(self):
         return self.seq
 
     def rotate(self, xy, radians, orig_x, orig_y):
-        """apply a rotation to xy around the center""" # TODO - could pass list of vertices
-        x, y = xy
-        ax, ay = x-orig_x, y-orig_y
+        """apply a rotation to xy around the center"""  # TODO - could pass list of vertices
+        ax, ay = xy[0] - orig_x, xy[1] - orig_y
         cos_rad = math.cos(radians)
         sin_rad = math.sin(radians)
         return (orig_x + cos_rad * ax + sin_rad * ay,
                 orig_y - sin_rad * ax + cos_rad * ay)
-
-    def remove_edge(self):
-        if self.poly:
-            LOG.info(self)
-            self.poly.set(lw=THIN_EDGE_LINE_WIDTH)
-        #else:
-            #breakpoint()
-
-    def add_edge(self):
-        if self.poly:
-            LOG.info(self)
-            self.poly.set(lw=WIDE_EDGE_LINE_WIDTH)
 
     def get_points(self):
         """Given size and center, return vertices; also move to top with zorder"""
@@ -93,13 +78,14 @@ class Shape():
         x, y = self.xy
         s2 = self.size
         if self.which == 'T':
-            points = [(x, y+s2), (x+s2, y-s2), (x-s2, y-s2)]
+            points = [(x, y + s2), (x + s2, y - s2), (x - s2, y - s2)]
         elif self.which == 'S':
-            points = [(x-s2, y-s2), (x-s2, y+s2), (x+s2, y+s2), (x+s2, y-s2)]
+            points = [(x - s2, y - s2), (x - s2, y + s2),
+                      (x + s2, y + s2), (x + s2, y - s2)]
         else:
             return None
 
-        if self.angle != 0:
+        if self.angle:
             radians = self.angle * PI / 180
             points = [self.rotate(xy, radians, x, y) for xy in points]
         return points
@@ -112,11 +98,10 @@ class Shape():
         elif self.fillKind >= 2:
             # for patterns, edge_color controls the hash lines
             fc, ec = COLOR_MAP['WHITE'], self.color
-        else: # transparent
+        else:  # transparent
             fc, ec = COLOR_MAP['WHITE'], COLOR_MAP['BLUE']
         LOG.debug(f'{self=} {ec=} {fc=}')
         return fc, ec
-
 
     def create_circle_polygon(self):
         """return a CirclePolygon"""
@@ -137,30 +122,26 @@ class Shape():
     def create_poly(self):  # TODO:opt use funcmap from which
         """create a matplot shape from a MPL shape"""
         if self.which == 'T':
-            self.poly = self.create_triangle()
+            poly = self.create_triangle()
         elif self.which == 'S':
             LOG.debug(f'SQUARE {self=}')
-            self.poly = self.create_square()
+            poly = self.create_square()
         elif self.which == 'C':
-            self.poly = self.create_circle()
+            poly = self.create_circle()
         elif self.which == 'CP':
-            self.poly = self.create_circle_polygon()
+            poly = self.create_circle_polygon()
         else:
             LOG.error(f"unknown shape type {self=}")
             return None
         fc, ec = self.get_face_and_edge_color()
-        self.poly.set(ec=ec, fc=fc, hatch=HATCH_MAP[self.fillKind], 
-            lw=WIDE_EDGE_LINE_WIDTH, zorder=self.zorder)
-        return self.poly
+        poly.set(ec=ec, fc=fc,
+                 hatch=HATCH_MAP[self.fillKind], zorder=self.zorder)
+        return poly
 
     def __repr__(self):
         s = f'Shape:{self.which} seq:{self.seq} {self.xy} {self.size} {self.scolor} Z:{self.zorder}'
-        if self.poly is None:
-            s += ' poly: None'
         if self.fillKind:
             s += f' fill:{self.fillKind}'
         if self.angle:
             s += f' angle:{self.angle}'
         return s
-
-
--- src/subAPI.py
+++ src/subAPI.py
@@ -13,19 +13,16 @@
 
 # python imports
 import argparse
-#from collections import defaultdict
-import copy
 import logging
-import math
 import os
 import sys
 import time
 
 # animation imports
-###import PySide2 # for PDB operation
 import matplotlib
+# TODO - use Qt5 if not on PC
 if os.name != 'nt':
-    matplotlib.use('Qt5Agg')
+    matplotlib.use('Qt5Agg')  # must precede pyplot
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
@@ -37,6 +34,9 @@ from Shape import Shape
 
 LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.INFO)
+
+WIDE_EDGE_LINE_WIDTH, THIN_EDGE_LINE_WIDTH = 3, 1
+
 
 def create_matplot(args, box_title):
     """init all the figure attributes - some is Mac-specific, some must be done b4 creating fig"""
@@ -64,7 +64,8 @@ def create_matplot(args, box_title):
     x, y, dx, dy = mngr.window.geometry().getRect()
     HGAP, VGAP = 30, 85
     Y, HSP = VGAP + dy, HGAP+5
-    coord = [ (0, 0), (dx+HGAP, 0), (2*dx+HSP, 0), (0, Y), (dx+HGAP, Y), (2*dx+HSP, Y) ]
+    coord = [(0, 0), (dx+HGAP, 0), (2*dx+HSP, 0),
+             (0, Y), (dx+HGAP, Y), (2*dx+HSP, Y)]
     x, y = coord[args.index-1]
     LOG.debug(f'{x=}, {y=}')
     mngr.window.setGeometry(x, y, int(dx), int(dy))
@@ -76,6 +77,7 @@ def create_matplot(args, box_title):
     #image = axes.imshow(image, extent=[MARX, args.graphx-MARX, MARY, args.graphy-MARY])
 
     return fig, axes
+
 
 def init_dds(domain_id, qos_file, qos_lib, qos_profile, extended=True):
     """return reader_dic and info_dic"""  # TODO add Pubs here
@@ -90,12 +92,16 @@ def init_dds(domain_id, qos_file, qos_lib, qos_profile, extended=True):
 
     circle_topic = dds.DynamicData.Topic(participant, "Circle", provider_type)
     square_topic = dds.DynamicData.Topic(participant, "Square", provider_type)
-    triangle_topic = dds.DynamicData.Topic(participant, "Triangle", provider_type)
+    triangle_topic = dds.DynamicData.Topic(
+        participant, "Triangle", provider_type)
 
     reader_qos = provider.datareader_qos
-    circle_reader = dds.DynamicData.DataReader(subscriber, circle_topic, reader_qos)
-    square_reader = dds.DynamicData.DataReader(subscriber, square_topic, reader_qos)
-    triangle_reader = dds.DynamicData.DataReader(subscriber, triangle_topic, reader_qos)
+    circle_reader = dds.DynamicData.DataReader(
+        subscriber, circle_topic, reader_qos)
+    square_reader = dds.DynamicData.DataReader(
+        subscriber, square_topic, reader_qos)
+    triangle_reader = dds.DynamicData.DataReader(
+        subscriber, triangle_topic, reader_qos)
 
     reader_dic = {
         'C': circle_reader,
@@ -110,8 +116,10 @@ def init_dds(domain_id, qos_file, qos_lib, qos_profile, extended=True):
 
     return reader_dic, info_dic
 
+
 class InstanceGen:
     """returns instance number confined to the range"""
+
     def __init__(self, number):
         self.number = number
         self.at = 0
@@ -121,26 +129,32 @@ class InstanceGen:
         """return the last given number"""
         return self.at
 
+    def get_prev_ix(self):
+        return (self.at - 1 + self.number) % self.number
+
     def next(self):
         self.at = self.instance
         self.instance = (self.instance + 1) % self.number
         return self.at
 
+
 def form_instance_gen_key(topic_letter, color):
     return f'{topic_letter}-{color}'
+
 
 def form_poly_key(which, color, instance_num):
     return f'{which}-{color}-{instance_num}'
 
+
 def main(args):
 
-    poly_dic = {} # all polygon instances keyed by Topic+Color+InstanceNum
+    poly_dic = {}  # all polygon instances keyed by Topic+Color+InstanceNum
     #poly_list = []
 
     # keep shape dic for each Topic holding list of instances
     #shapes = {'C': defaultdict(list), 'S': defaultdict(list), 'T': defaultdict(list)}
     shapes = {'C': {}, 'S': {}, 'T': {}}
-    instance_gen_dic = {} # Topic-color: InstanceGen
+    instance_gen_dic = {}  # Topic-color: InstanceGen
 
     title = f"ShapeSubscriber Domain:{args.domain_id} Slot: {args.index}"
     fig, axes = create_matplot(args, box_title=title)
@@ -149,31 +163,38 @@ def main(args):
     cwd = os.path.dirname(os.path.realpath(__file__))
 
     reader_dic, info_dic = init_dds(
-	domain_id=args.domain_id, 
-        qos_file=cwd + args.qos_file[1:] if args.qos_file[0] == '.' else args.qos_file,
-        qos_lib=args.qos_lib, 
+        domain_id=args.domain_id,
+        qos_file=cwd +
+        args.qos_file[1:] if args.qos_file[0] == '.' else args.qos_file,
+        qos_lib=args.qos_lib,
         qos_profile=args.qos_profile,
         extended=args.extended
     )
 
-    def process_shape(which, shape, ix):
+    def handle_one_sample(which, sample):
         """update the poly_dic with fresh shape info"""
-        if args.justdds:
-            LOG.info("early exit from _process_sample")
-            return
-
-        LOG.info(f"SHAPE: {shape=}, {ix=}")
+        """create/update a matplotlib polygon from the sample data, add to poly_dic
+           remove the prior poly's edge"""
+        shape = Shape(args=args, which=which,
+                      data=sample.data, info=sample.info)
+        instance_gen_key = form_instance_gen_key(
+            which, shape.scolor)  # use API to get Key
+        inst = instance_gen_dic.get(instance_gen_key)
+        if not inst:
+            inst = InstanceGen(info_dic[which])
+            instance_gen_dic[instance_gen_key] = inst
+        ix = inst.next()
+        LOG.debug(f"SHAPE: {shape=}, {ix=}")
         poly_key = form_poly_key(which, shape.scolor, ix)
         poly = poly_dic.get(poly_key)
+        if args.justdds:
+            LOG.info("early exit")
+            return
         if not poly:
             poly = shape.create_poly()
-            #LOG.info(f'{shape.poly.get_ec()=}')
             poly_dic[poly_key] = poly
-            LOG.info(f"added {poly_key=}, {shape.scolor}")
-            if not args.justdds:
-                axes.add_patch(poly)
-        else:
-            shape.add_edge()
+            LOG.info(f"added {poly_key=}")
+            axes.add_patch(poly)
 
         xy = shape.get_points()
         if which == 'CP':
@@ -184,54 +205,35 @@ def main(args):
             poly.set(center=xy)
         else:
             poly.set_xy(xy)
-        poly.set(zorder=shape.zorder)
+        poly.set(lw=WIDE_EDGE_LINE_WIDTH, zorder=shape.zorder)
 
-    def get_sample(reader, which):
-        """get samples and create shapes"""
-        """take all valid samples -
-           create a shape (with edge) from the sample data, add to shape list
-           remove the prior shape's edge
-           process shapes into polygons """
+        prev_poly_key = form_poly_key(which, shape.scolor, inst.get_prev_ix())
+        prev_poly = poly_dic.get(prev_poly_key)
+        if prev_poly:
+            prev_poly.set(lw=THIN_EDGE_LINE_WIDTH)
+
+    def handle_samples(reader, which):
+        """get samples and handle each"""
 
         with reader.take_valid() as samples:
             for sample in samples:
-
-                shape = Shape(args=args, which=which, data=sample.data, info=sample.info)
-                instance_gen_key = form_instance_gen_key(which, shape.scolor)  # use API to get Key
-                inst = instance_gen_dic.get(instance_gen_key)
-                if not inst:
-                    inst = InstanceGen(info_dic[which])
-                    instance_gen_dic[instance_gen_key] = inst
-                ix = inst.next()
-                if not shapes[which].get(shape.scolor):
-                    LOG.info(f'NEW COLOR {shape.scolor} {info_dic[which]}')
-                    shapes[which][shape.scolor] = [None] * info_dic[which]
-                #shapes[which][shape.scolor][ix] = copy.deepcopy(shape)
-                shapes[which][shape.scolor][ix] = shape
-                process_shape(which, shape, ix)
-                prev_shape = shapes[which][shape.scolor][ix-1]
-                LOG.info(f'{prev_shape=} {ix=}')
-                if prev_shape:
-                    prev_shape.remove_edge()
+                handle_one_sample(which, sample)
 
         if args.nap:
             time.sleep(args.nap)
             LOG.info(f'Sleeping {args.nap=}')
 
-
     def fetch_and_draw(_frame):
         """The animation function, called periodically in a set interval, reads the
         last image received and draws it"""
-        ###if _frame > 500: sys.exit(0)
         readers = reader_dic.values()
         whiches = reader_dic.keys()
         for reader, which in zip(readers, whiches):
             #LOG.info(f'FOR READERS {which=} {len(readers)=}')
-            get_sample(reader, which)
+            handle_samples(reader, which)
 
         return poly_dic.values()  # give back the updated values so they are rendered
-        #return poly_list  # give back the updated values so they are rendered
-
+        # return poly_list  # give back the updated values so they are rendered
 
     if args.justdds:
         LOG.info(f'RUNNING {args.justdds=} reads')
@@ -243,47 +245,49 @@ def main(args):
     # Create the animation and show
     else:
         # lower interval if updates are jerky
-        #ani = FuncAnimation(fig, fetch_and_draw, interval=10, blit=True )
-        ani = FuncAnimation(fig, fetch_and_draw, interval=20, blit=True )
-
+        ani = FuncAnimation(fig, fetch_and_draw, interval=20, blit=True)
         # Show the image and block until the window is closed
         plt.show()
     LOG.info("Exiting...")
 
+
 def parse_args(args):
     """pass args to allow testing"""
-    FIGX, FIGY = 2.375, 2.72 # match RTI ShapesDemo box size
+    FIGX, FIGY = 2.375, 2.72  # match RTI ShapesDemo box size
     MAXX, MAXY = 240, 270
     DEFAULT_QOS_FILE = './SimpleShape.xml'
     DEFAULT_QOS_LIB, DEFAULT_QOS_PROFILE = 'MyQoSLib', 'MyQoSProfile'
     DEFAULT_HISTORY = 6
 
-    parser = argparse.ArgumentParser(description="Simple ShapesDemo Subscriber")
+    parser = argparse.ArgumentParser(
+        description="Simple ShapesDemo Subscriber")
     parser.add_argument('--domain_id', '-d', type=int, default=0,
-        help='Specify Domain on which to listen [0]-122')
+                        help='Specify Domain on which to listen [0]-122')
     parser.add_argument('--extended', action=argparse.BooleanOptionalAction,
-        help='Use ShapeTypeExtended [ShapeType]')
+                        help='Use ShapeTypeExtended [ShapeType]')
     parser.add_argument('-f', '--figureXY', default=(FIGX, FIGY), type=int, nargs=2,
-        help=f'x,y of figure in inches [{FIGX},{FIGY}]')
+                        help=f'x,y of figure in inches [{FIGX},{FIGY}]')
     parser.add_argument('-g', '--graphXY', default=(MAXX, MAXY), type=int, nargs=2,
-        help=f'width and height of graph in pixels [{MAXX},{MAXY}]')
+                        help=f'width and height of graph in pixels [{MAXX},{MAXY}]')
     parser.add_argument('-i', '--index', type=int, default=1,
-        help=f'screen slot index [1]-6')
+                        help=f'screen slot index [1]-6')
     parser.add_argument('--justdds', '-j', type=int,
-        help='just call dds draw this many times, no graphing, for testing')
+                        help='just call dds draw this many times, no graphing, for testing')
     parser.add_argument('-l', '--level', type=int,
-        help="logger level [4=INFO]")
+                        help="logger level [4=INFO]")
     parser.add_argument('--nap', '-n', type=float, default=0,
-        help=f'intrasample naptime [default:0.0]')
+                        help=f'intrasample naptime [default:0.0]')
     parser.add_argument('--qos_file', '-qf', type=str, default=DEFAULT_QOS_FILE,
-        help=f'full path of QoS file [{DEFAULT_QOS_FILE}]')
+                        help=f'full path of QoS file [{DEFAULT_QOS_FILE}]')
     parser.add_argument('--qos_lib', '-ql', type=str, default=DEFAULT_QOS_LIB,
-        help=f'QoS library name [{DEFAULT_QOS_LIB}]')
+                        help=f'QoS library name [{DEFAULT_QOS_LIB}]')
     parser.add_argument('--qos_profile', '-qp', type=str, default=DEFAULT_QOS_PROFILE,
-        help=f'QoS profile name [{DEFAULT_QOS_PROFILE}]')
-    parser.add_argument('--square-history-depth', '-shd', type=int,
-        help=f'history depth for square topic [{DEFAULT_HISTORY}]')
+                        help=f'QoS profile name [{DEFAULT_QOS_PROFILE}]')
+    parser.add_argument('--publish', '-p', type=str,
+                        help=f'publish a Shape (Square, Triangle, Circle) \nColor-Speed [Blue-Normal] Colors: Blue, Green, Red, Orange, Magenta, Yellow')
 
+    parser.add_argument('--square-history-depth', '-shd', type=int,
+                        help=f'history depth for square topic [{DEFAULT_HISTORY}]')
 
     args = parser.parse_args()
     args.graphx, args.graphy = args.graphXY
@@ -303,4 +307,4 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         LOG.info("all done")
 
-## No need for --extended, subscriber just uses extended type
+# No need for --extended, subscriber just uses extended type
