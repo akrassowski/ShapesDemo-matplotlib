1bda57e4f2a533b3c7d1f9b12f5ba72902a6c2f2
andy krassowski <akrassowski@gmail.com>
2022-10-04 16:46:31 -0400

    fix reader_dic, drop info_dic, pass args deeper

--- src/subAPI.py
+++ src/subAPI.py
@@ -35,7 +35,8 @@ from Shape import Shape
 LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.INFO)
 
-WIDE_EDGE_LINE_WIDTH, THIN_EDGE_LINE_WIDTH = 3, 1
+WIDE_EDGE_LINE_WIDTH, THIN_EDGE_LINE_WIDTH = 2, 1
+DEFAULT_TITLE = "Shapes"
 
 
 def create_matplot(args, box_title):
@@ -70,118 +71,99 @@ def create_matplot(args, box_title):
     LOG.debug(f'{x=}, {y=}')
     mngr.window.setGeometry(x, y, int(dx), int(dy))
 
-    # set a background image
+    # set a background image  # TODO scale when resizing or specify "center"?
     image = plt.imread("RTI_Logo_RGB-Color.png")
     fig.figimage(image, xo=35, yo=120, zorder=3, alpha=0.1)
-    #MARX, MARY = 25, 55
-    #image = axes.imshow(image, extent=[MARX, args.graphx-MARX, MARY, args.graphy-MARY])
 
     return fig, axes
 
 
-def init_dds(domain_id, qos_file, qos_lib, qos_profile, extended=True):
-    """return reader_dic and info_dic"""  # TODO add Pubs here
-    LOG.info(f'init_dds {domain_id=} {qos_file} {extended=}')
-    participant = dds.DomainParticipant(domain_id)
+def _init_dds(args):
+    participant = dds.DomainParticipant(args.domain_id)
+    qos_provider = get_qos_provider(args.qos_file, args.qos_lib, args.qos_profile)
+
+    type_name = "ShapeTypeExtended" if args.extended else "ShapeType"
+    provider_type = qos_provider.type(type_name)
+    topic_dic = {
+        'C': dds.DynamicData.Topic(participant, "Circle", provider_type),
+        'S': dds.DynamicData.Topic(participant, "Square", provider_type),
+        'T': dds.DynamicData.Topic(participant, "Triangle", provider_type)
+    }
+    return participant, qos_provider, topic_dic
+
+def get_max_samples_per_instance(reader):
+    """ helper to fetch depth from a reader"""
+    return reader.qos.resource_limits.max_samples_per_instance
+
+def init_dds_pub(args):
+    participant, qos_provider, topic_dic = _init_dds(args)
+    #TODO more here
+    writer_dic = {
+    }
+    return writer_dic
+
+def init_dds_sub(args):
+    """return reader_dic"""
+    participant, qos_provider, topic_dic = _init_dds(args)
     subscriber = dds.Subscriber(participant)
-
-    provider = dds.QosProvider(qos_file, "MyQosLibrary::MyProfile")
-
-    type_name = "ShapeTypeExtended" if extended else "ShapeType"
-    provider_type = provider.type(type_name)
-
-    circle_topic = dds.DynamicData.Topic(participant, "Circle", provider_type)
-    square_topic = dds.DynamicData.Topic(participant, "Square", provider_type)
-    triangle_topic = dds.DynamicData.Topic(
-        participant, "Triangle", provider_type)
-
-    reader_qos = provider.datareader_qos
-    circle_reader = dds.DynamicData.DataReader(
-        subscriber, circle_topic, reader_qos)
-    square_reader = dds.DynamicData.DataReader(
-        subscriber, square_topic, reader_qos)
-    triangle_reader = dds.DynamicData.DataReader(
-        subscriber, triangle_topic, reader_qos)
-
-    reader_dic = {
-        'C': circle_reader,
-        'S': square_reader,
-        'T': triangle_reader,
-    }
-    info_dic = {
-        'C': circle_reader.qos.resource_limits.max_samples_per_instance, 'CI': 0,
-        'S': square_reader.qos.resource_limits.max_samples_per_instance, 'SI': 0,
-        'T': triangle_reader.qos.resource_limits.max_samples_per_instance, 'TI': 0,
-    }
-
-    return reader_dic, info_dic
+    reader_qos = qos_provider.datareader_qos
+    reader_dic = {}
+    for which in args.subscribe:
+        LOG.info(f'Subscribing to {which} {topic_dic[which]=}')
+        reader_dic[which] = dds.DynamicData.DataReader(subscriber, topic_dic[which], reader_qos)
+ 
+    return reader_dic
 
 
 class InstanceGen:
-    """returns instance number confined to the range"""
+    """returns instance index confined to the range"""
 
-    def __init__(self, number):
-        self.number = number
+    def __init__(self, _range):
+        self._range = _range
         self.at = 0
         self.instance = 0
 
-    def at(self):
-        """return the last given number"""
-        return self.at
-
     def get_prev_ix(self):
-        return (self.at - 1 + self.number) % self.number
+        """return the previous given number with no changes"""
+        return (self.at - 1 + self._range) % self._range
 
     def next(self):
+        """return the next instance index and move along"""
         self.at = self.instance
-        self.instance = (self.instance + 1) % self.number
+        self.instance = (self.instance + 1) % self._range
         return self.at
-
-
-def form_instance_gen_key(topic_letter, color):
-    return f'{topic_letter}-{color}'
 
 
 def form_poly_key(which, color, instance_num):
     return f'{which}-{color}-{instance_num}'
 
-
-def main(args):
-
-    poly_dic = {}  # all polygon instances keyed by Topic+Color+InstanceNum
-    #poly_list = []
-
-    # keep shape dic for each Topic holding list of instances
-    #shapes = {'C': defaultdict(list), 'S': defaultdict(list), 'T': defaultdict(list)}
-    shapes = {'C': {}, 'S': {}, 'T': {}}
-    instance_gen_dic = {}  # Topic-color: InstanceGen
-
-    title = f"ShapeSubscriber Domain:{args.domain_id} Slot: {args.index}"
-    fig, axes = create_matplot(args, box_title=title)
-
-    # fig.suptitle("")
+def get_qos_provider(qos_file, qos_lib, qos_profile):
+    """fetch the qos_profile from the lib in the file"""
     cwd = os.path.dirname(os.path.realpath(__file__))
+    #  prepend with cwd if starts with dot
+    qos_file = cwd + qos_file[1:] if qos_file[0] == '.' else qos_file
+    return dds.QosProvider(qos_file, f'{qos_lib}::{qos_profile}')
 
-    reader_dic, info_dic = init_dds(
-        domain_id=args.domain_id,
-        qos_file=cwd +
-        args.qos_file[1:] if args.qos_file[0] == '.' else args.qos_file,
-        qos_lib=args.qos_lib,
-        qos_profile=args.qos_profile,
-        extended=args.extended
-    )
+global sample_count
+sample_count = 0
+def start_subscriber(args, fig, axes):
+    """First, some globals and helpers"""
+    poly_dic = {}  # all polygon instances keyed by Topic+Color+InstanceNum
+    instance_gen_dic = {}  # Topic-color: InstanceGen
+    reader_dic = init_dds_sub(args)
 
     def handle_one_sample(which, sample):
         """update the poly_dic with fresh shape info"""
         """create/update a matplotlib polygon from the sample data, add to poly_dic
            remove the prior poly's edge"""
+        global sample_count
+        sample_count += 1
         shape = Shape(args=args, which=which,
                       data=sample.data, info=sample.info)
-        instance_gen_key = form_instance_gen_key(
-            which, shape.scolor)  # use API to get Key
+        instance_gen_key = f'{which}-{shape.scolor}'  # TODO use API to get Key
         inst = instance_gen_dic.get(instance_gen_key)
         if not inst:
-            inst = InstanceGen(info_dic[which])
+            inst = InstanceGen(get_max_samples_per_instance(reader_dic[which]))
             instance_gen_dic[instance_gen_key] = inst
         ix = inst.next()
         LOG.debug(f"SHAPE: {shape=}, {ix=}")
@@ -215,6 +197,7 @@ def main(args):
     def handle_samples(reader, which):
         """get samples and handle each"""
 
+        LOG.debug(f'START {sample_count=}')
         with reader.take_valid() as samples:
             for sample in samples:
                 handle_one_sample(which, sample)
@@ -222,6 +205,8 @@ def main(args):
         if args.nap:
             time.sleep(args.nap)
             LOG.info(f'Sleeping {args.nap=}')
+        
+        LOG.info(f'{sample_count=}')
 
     def fetch_and_draw(_frame):
         """The animation function, called periodically in a set interval, reads the
@@ -229,13 +214,12 @@ def main(args):
         readers = reader_dic.values()
         whiches = reader_dic.keys()
         for reader, which in zip(readers, whiches):
-            #LOG.info(f'FOR READERS {which=} {len(readers)=}')
             handle_samples(reader, which)
 
         return poly_dic.values()  # give back the updated values so they are rendered
-        # return poly_list  # give back the updated values so they are rendered
 
-    if args.justdds:
+
+    if args.justdds:  # just for debugging
         LOG.info(f'RUNNING {args.justdds=} reads')
         for i in range(args.justdds):
             fetch_and_draw(10)
@@ -245,9 +229,34 @@ def main(args):
     # Create the animation and show
     else:
         # lower interval if updates are jerky
+        LOG.info('start animation')
         ani = FuncAnimation(fig, fetch_and_draw, interval=20, blit=True)
         # Show the image and block until the window is closed
         plt.show()
+
+def start_publisher(args, fig, axes):
+    print("Publishing is TBD")
+    sys.exit(0)
+
+def main(args):
+
+    if args.title == DEFAULT_TITLE:
+        title = f"Shapes Domain:{args.domain_id}"
+    else:
+        title = args.title
+    fig, axes = create_matplot(args, box_title=title)
+
+    if args.subtitle:
+        fig.suptitle(args.subtitle)
+
+    if args.subscribe:
+        start_subscriber(args, fig, axes)
+    elif args.publish:
+        start_publisher(args, fig, axes)
+    else:
+        print("Must run as either publisher or subscriber")
+        sys.exit(-1)
+
     LOG.info("Exiting...")
 
 
@@ -256,11 +265,20 @@ def parse_args(args):
     FIGX, FIGY = 2.375, 2.72  # match RTI ShapesDemo box size
     MAXX, MAXY = 240, 270
     DEFAULT_QOS_FILE = './SimpleShape.xml'
-    DEFAULT_QOS_LIB, DEFAULT_QOS_PROFILE = 'MyQoSLib', 'MyQoSProfile'
+    DEFAULT_QOS_LIB, DEFAULT_QOS_PROFILE = 'MyQosLibrary', 'MyProfile'
     DEFAULT_HISTORY = 6
 
+    def validate_shape_letters(letters):
+        if len(letters) > 3:
+            raise ValueError('Cannot subscribe to more then 3 Topics')
+        for letter in letters:
+            if letter.upper() not in "CST":
+                raise ValueError('Topic letters ({letter}) must be one or more of: ' + 
+                                 '"CST" for Circle, Square, Triangle')
+        return letters.upper()
+
     parser = argparse.ArgumentParser(
-        description="Simple ShapesDemo Subscriber")
+        description="Simple ShapesDemo")
     parser.add_argument('--domain_id', '-d', type=int, default=0,
                         help='Specify Domain on which to listen [0]-122')
     parser.add_argument('--extended', action=argparse.BooleanOptionalAction,
@@ -271,23 +289,32 @@ def parse_args(args):
                         help=f'width and height of graph in pixels [{MAXX},{MAXY}]')
     parser.add_argument('-i', '--index', type=int, default=1,
                         help=f'screen slot index [1]-6')
-    parser.add_argument('--justdds', '-j', type=int,
-                        help='just call dds draw this many times, no graphing, for testing')
     parser.add_argument('-l', '--level', type=int,
                         help="logger level [4=INFO]")
-    parser.add_argument('--nap', '-n', type=float, default=0,
-                        help=f'intrasample naptime [default:0.0]')
     parser.add_argument('--qos_file', '-qf', type=str, default=DEFAULT_QOS_FILE,
                         help=f'full path of QoS file [{DEFAULT_QOS_FILE}]')
     parser.add_argument('--qos_lib', '-ql', type=str, default=DEFAULT_QOS_LIB,
                         help=f'QoS library name [{DEFAULT_QOS_LIB}]')
     parser.add_argument('--qos_profile', '-qp', type=str, default=DEFAULT_QOS_PROFILE,
                         help=f'QoS profile name [{DEFAULT_QOS_PROFILE}]')
-    parser.add_argument('--publish', '-p', type=str,
-                        help=f'publish a Shape (Square, Triangle, Circle) \nColor-Speed [Blue-Normal] Colors: Blue, Green, Red, Orange, Magenta, Yellow')
+    parser.add_argument('--subtitle', '-st', type=str, default="",
+                        help=f'Provide a subtitle to the screen ["Slot n"]')
+    parser.add_argument('--title', '-t', type=str, default=DEFAULT_TITLE,
+                        help=f'Provide a title to the widget [DEFAULT_TITLE]')
 
+    parser.add_argument('--subscribe', '-sub', type=validate_shape_letters, default="S",
+                        help=f'simple subscriber to any of Circle, Square, Triangle [S]')
     parser.add_argument('--square-history-depth', '-shd', type=int,
                         help=f'history depth for square topic [{DEFAULT_HISTORY}]')
+
+    parser.add_argument('--publish', '-pub', type=validate_shape_letters,
+                        help=f'simple publisher of any of Circle, Square, Triangle [S]')
+    
+    ## internal args used whilst developing/debugging only
+    parser.add_argument('--justdds', '-j', type=int,
+                        help='just call dds draw this many times, no graphing, for testing')
+    parser.add_argument('--nap', '-n', type=float, default=0,
+                        help=f'intrasample naptime [default:0.0]')
 
     args = parser.parse_args()
     args.graphx, args.graphy = args.graphXY
@@ -308,3 +335,31 @@ if __name__ == "__main__":
         LOG.info("all done")
 
 # No need for --extended, subscriber just uses extended type
+
+"""
+OPTIONS
+  Shape - C, S, T
+  
+  SUB-only
+    sleep
+    history depth (QoS)
+
+  PUB-only
+    Color - 1 of 8
+    size
+    angle
+    fill
+    delta xy
+    delta angle
+
+
+{ 'triangle': {
+  'color': red
+  size: 30,
+  fill: none
+  angle: 
+
+
+
+CR
+"""
