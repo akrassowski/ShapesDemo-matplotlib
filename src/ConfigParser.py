419227711bf81ad9036a6f0d1b618a9db16b1cdd
andy krassowski <akrassowski@gmail.com>
2022-11-26 00:52:22 -0500

    pylint

--- src/ConfigParser.py
+++ src/ConfigParser.py
@@ -53,6 +53,7 @@ class ConfigParser:
         self.init_defaults(defaults)
 
     def init_defaults(self, defaults):
+        """Helper to initialize a shape's default values as byproduct of help"""
         self.defaults = defaults
         self._pub_default_and_help('xy', [50, 50], 'Starting xy position')
         self._pub_default_and_help(
@@ -64,7 +65,7 @@ class ConfigParser:
         self._pub_default_and_help('color', defaults['COLOR'], 'Shape color: ' + colors)
         self._pub_default_and_help('shapesize', 30, 'Size of shape in pixels')
         self._pub_default_and_help('fillKind', 0,
-                                  ('Fill pattern of the ShapeExtendedType: ' + 
+                                  ('Fill pattern of the ShapeExtendedType: ' +
                                    '[0 (solid)], 1 (empty), 2 (horizontal), 3 (vertical)'))
         self._pub_default_and_help('angle', 0, 'Starting angle for ShapeExtendedType')
         self._pub_default_and_help(
@@ -104,16 +105,17 @@ class ConfigParser:
 
     ### Checkers and normalizers
     @staticmethod
-    def _err_msg(param, desired, value):
-        #return f'argument to {param} must be {desired} not {value}'
+    def err_msg_(param, desired, value):
         return f'{param} must be {desired} not {value}'
 
     @staticmethod
     def normalize_shape(shape):
+        """@return the normalized shape letter"""
         letter = shape[0].upper()
-        valid_letters = ['C', 'S', 'T']
+        valid_letters = ['CST']
         if letter not in valid_letters:
-            raise ValueError(self._err_msg('Shape first letter',  f'in {valid_letters}', shape))
+            raise ValueError(
+                ConfigParser.err_msg_('Shape first letter',  f'in {valid_letters}', shape))
         return letter
 
     @staticmethod
@@ -136,19 +138,20 @@ class ConfigParser:
                     normalized = ix
                     break
             else:  # python for..else
-                ValueError(self._err_msg('fillKind', f'in: {FILL_KIND_LIST}', txt))
+                ValueError(ConfigParser.err_msg_('fillKind', f'in: {FILL_KIND_LIST}', txt))
         else:
             try:
                 normalized = int(txt)
             except Exception as exc:
-                raise ValueError(self._err_msg('fillKind', f'in range 0-{len(FILL_KIND_LIST)}', txt)) from exc
+                phrase = f'in range 0-{len(FILL_KIND_LIST)}'
+                raise ValueError(ConfigParser.err_msg_('fillKind', phrase, txt)) from exc
         return normalized
 
     @staticmethod
-    def unused_normalize_bool(param, u_value):
+    def unused_normalize_bool(u_value):
         """Allow 'False' and '0' to be False"""
         normalized = bool(u_value)
-        if u_value == 'FALSE' or u_value == '0':
+        if u_value in ('FALSE', '0'):
             normalized = False
         return normalized
 
@@ -158,7 +161,7 @@ class ConfigParser:
         try:
             normalized = int(value)
         except Exception as exc:
-            raise ValueError(self._err_msg(param, "an integer", value)) from exc
+            raise ValueError(ConfigParser.err_msg_(param, "an integer", value)) from exc
         return normalized
 
     @staticmethod
@@ -167,22 +170,20 @@ class ConfigParser:
         try:
             normalized = float(value)
         except Exception as exc:
-            raise ValueError(self._err_msg(param, "a float", value)) from exc
+            raise ValueError(ConfigParser.err_msg_(param, "a float", value)) from exc
         return normalized
 
     def normalize_xy(self, param, value):
         """Raise if there's something wrong"""
         if (not isinstance(value, tuple) and not isinstance(value, list)) or len(value) != 2:
-        #if (not isinstance(value, list)) or len(value) != 2:
-            raise ValueError(self._err_msg(param, "a (x, y) list", value))
-        LOG.debug(f'{param=} {value=} {type(value)=} {type(value[0])=}')
+            raise ValueError(self.err_msg_(param, "a (x, y) list", value))
         try:
             normalized = [int(value[0]), int(value[1])]  # list not tuple so updates can be assigned
         except Exception as exc:
-            raise ValueError(self._err_msg(param, "a [x, y] list of int", value)) from exc
+            raise ValueError(self.err_msg_(param, "a [x, y] list of int", value)) from exc
         return normalized
     # end Checkers and normalizers
-    
+
     def get_entity(self, act):
         """Expect pubX or subY; return PUB, X or SUB, Y"""
         split_result = act.split(":")
@@ -190,7 +191,7 @@ class ConfigParser:
         u_act = action[0:3].upper()
         #print(f'{action=} {name=} {u_act=}')
         if u_act in "SUBPUB":
-           action = u_act
+            action = u_act
         else:
             raise ValueError(f'entity must start with "pub" or "sub" not {act}')
         return action, name
@@ -203,8 +204,8 @@ class ConfigParser:
             dic, count = {}, 1
             for key, value in ordered_pairs:
                 if key in dic:
-                  dic[f'{key}_{count}'] = value
-                  count += 1
+                    dic[f'{key}_{count}'] = value
+                    count += 1
                 else:
                     dic[key] = value
             return dic
@@ -217,20 +218,21 @@ class ConfigParser:
         elif isinstance(stream_or_fname, StringIO):
             cfg_dic = json.loads(stream_or_fname.getvalue(), object_pairs_hook=_dict_decorate)
         else:
-            raise ValueError(self._err_msg('param', 'must be StringIO or str', type(stream_or_fname)))
+            raise ValueError(
+                self.err_msg_('param', 'must be StringIO or str', type(stream_or_fname)))
         for key in cfg_dic.keys():
             if "PUB" in key.upper():
                 self.parse_pub(cfg_dic[key], key)
-            elif "SUB" == action:
+            elif "SUB" in key.upper():
                 self.parse_sub(cfg_dic[key], key)
 
     def parse_sub(self, cfg, entity_name):
         """parse the sub dictionary"""
-        LOG.debug(f'sub {cfg=} {entity_name=}')
+        LOG.debug('cfg:%s entity_name:%s', cfg, entity_name)
         for shape in cfg.keys():
             n_shape = self.normalize_shape(shape)
             self.sub_dic[n_shape] = self.sub_dic_default
-        LOG.debug(f'{self.sub_dic=}')
+        LOG.debug('sub_dic: %s', self.sub_dic)
 
 
     def _fixup_unknown(self, shape, color):
@@ -257,9 +259,9 @@ class ConfigParser:
         """parse the pub dictionary"""
         #LOG.info(f'pub {cfg=} {cfg.keys()=}')
         #print(f'pub {cfg=} {cfg.keys()=} {entity_name=}')
-                
+
         if len(cfg.keys()) > 1:
-            LOG.warning("Only the first shape's config will be used.  Publishing multiple shapes is TBD")
+            LOG.warning("Only shape 1's config will be used.  Publishing multiple shapes is TBD")
         for shape in cfg.keys():
             n_shape = self.normalize_shape(shape)
             key = ConnextPublisher.form_pub_key(n_shape, 'UNKNOWN')
@@ -319,7 +321,7 @@ class ConfigParser:
             for which in parsed_args.publish:
                 defaults, _ = self.get_pub_config(default=True)
                 config[which] = defaults
-                LOG.debug(f'{config[which]=}')
+                LOG.debug('config[which]:%s', config[which])
         else:
             LOG.error("Must run as either publisher or subscriber, terminating")
             sys.exit(-1)
