b1b84aa579d92a27318d45e0ad277e796f68f928
andy krassowski <akrassowski@gmail.com>
2023-01-11 23:56:54 -0500

    refactor pub_list

--- src/ConfigParser.py
+++ src/ConfigParser.py
@@ -24,15 +24,13 @@ LOG = logging.getLogger(__name__)
 
 # Strategy:
 # keep sub & pub separate even though only one is used at a time
-# key dic by which & color to allow for different settings by shape & color
+# key list of dic by which & color to allow for different settings by shape & color
 # Population
-# start with empty pub and sub dictionaries
-# apply any config file on top
-# if any shape entry
-# start with defaults
-# override with file values
-# override with any cmd-line values
-# mark either pub or sub active by clearing the other
+# parse into list of pub dic and single sub_dic
+# validate attributes - dup = error
+# fill in missing with defaults
+# cmd-line values are limited to default shape names
+# TODO: allow pub and sub in same instance?
 
 FILL_KIND_LIST = ['SOLID', 'EMPTY', 'HORIZONTAL', 'VERTICAL']
 VALID_SHAPE_LETTERS = 'CST'
@@ -41,67 +39,59 @@ class ConfigParser:
     """parse the JSON config file populating a sub or pub dic"""
 
     def __init__(self, defaults):
-        self.pub_dic = {}  # holds normalized publisher data  TODO: keyed by Shape-Color
-        self.pub_dic_default = {}
-        self.pub_dic_help = {}
+        self.pub_list = []  # list of pub dicts
+        self.pub_default_dic = {}
+        self.pub_help_dic = {}
         self.pub_attr = []
 
-        self.sub_dic = {}  # holds normalized subscriber data
-        self.sub_dic_default = {}
-        self.sub_dic_help = {}
+        self.sub_dic = {}
+        #self.sub_list = [{}]  # holds normalized subscriber data as 1 element list
+        self.sub_default_dic = {}
+        self.sub_help_dic = {}
         self.sub_attr = []
         self.init_defaults(defaults)
 
     def init_defaults(self, defaults):
         """Helper to initialize a shape's default values as byproduct of help"""
-        self.defaults = defaults
-        self._pub_default_and_help('xy', [50, 50], 'Starting xy position')
-        self._pub_default_and_help(
-            'delta_xy', [5, 5], 'Center x and y change per update')
+        self._default_and_help(self.pub_default_dic, self.pub_help_dic, self.pub_attr,
+            key='xy', value=[50, 50], help='Starting xy position')
+        self._default_and_help(self.pub_default_dic, self.pub_help_dic, self.pub_attr,
+            key='delta_xy', value=[5, 5], help='Center x and y change per update')
 
-        color_list = COLOR_MAP.keys()
         trans = str.maketrans("", "", "'dict_keys()[]")
-        colors = str(color_list).translate(trans)
-        self._pub_default_and_help('color', defaults['COLOR'], 'Shape color: ' + colors)
-        self._pub_default_and_help('shapesize', 30, 'Size of shape in pixels')
-        self._pub_default_and_help('fillKind', 0,
-                                  ('Fill pattern of the ShapeExtendedType: ' +
-                                   '[0 (solid)], 1 (empty), 2 (horizontal), 3 (vertical)'))
-        self._pub_default_and_help('angle', 0, 'Starting angle for ShapeExtendedType')
-        self._pub_default_and_help(
-            'delta_angle', 0, 'Rotation angle change per update for ShapeExtendedType')
-        # self.pub_default_and_help('reliability_type', 'best',
-        #                           'type of reliability: best, reliable')
+        colors = str(COLOR_MAP.keys()).translate(trans)
+        self._default_and_help(self.pub_default_dic, self.pub_help_dic, self.pub_attr,
+            key='color', value=defaults['COLOR'], help='Shape color: ' + colors)
+        self._default_and_help(self.pub_default_dic, self.pub_help_dic, self.pub_attr,
+            key='shapesize', value=30, help='Size of shape in pixels')
+        self._default_and_help(self.pub_default_dic, self.pub_help_dic, self.pub_attr,
+            key='fillKind', value=0, 
+            help=('Fill pattern of the ShapeExtendedType: ' +
+                  '[0 (solid)], 1 (empty), 2 (horizontal), 3 (vertical)'))
+        self._default_and_help(self.pub_default_dic, self.pub_help_dic, self.pub_attr,
+            key='angle', value=0, help='Starting angle for ShapeExtendedType')
+        self._default_and_help(self.pub_default_dic, self.pub_help_dic, self.pub_attr,
+            key='delta_angle', value=0, help='Rotation angle change per update for ShapeExtendedType')
 
-        # self.sub_default_and_help('reliability_type', 'best',
-        #                          'type of reliability: best, reliable')
-        self._sub_default_and_help('content_filter', None,
-                                   'Filter region tuple (top-left, bottom-right vertices)')
+        self._default_and_help(self.sub_default_dic, self.sub_help_dic, self.sub_attr,
+                               key='content_filter', value=None, 
+                               help='Filter region tuple (top-left, bottom-right vertices)')
 
     @staticmethod
-    def _default_and_help(value_dic, help_dic, key, value, help_text):
+    def _default_and_help(value_dic, help_dic, attr_list, key, value, help):
+        """add key, value, help to dictionaries"""
         value_dic[key] = value
-        help_dic[key] = help_text
-
-    def _sub_default_and_help(self, key, value, help_text):
-        """add key, value, help to dictionaries"""
-        self._default_and_help(self.sub_dic_default,
-                               self.sub_dic_help, key, value, help_text)
-        self.sub_attr.append(key)
-
-    def _pub_default_and_help(self, key, value, help_text):
-        """add key, value, help to dictionaries"""
-        self._default_and_help(self.pub_dic_default,
-                               self.pub_dic_help, key, value, help_text)
-        self.pub_attr.append(key)
+        help_dic[key] = help
+        attr_list.append(key)
 
     def get_pub_config(self, default=False):
         """public getter for pub config"""
-        return (self.pub_dic_default, self.pub_dic_help) if default else self.pub_dic
+        return (self.pub_default_dic, self.pub_help_dic) if default else self.pub_list
 
     def get_sub_config(self, default=False):
         """public getter for sub config"""
-        return (self.sub_dic_default, self.sub_dic_help) if default else self.sub_dic
+        ## return (self.sub_default_dic, self.sub_help_dic) if default else self.sub_list
+        return (self.sub_default_dic, self.sub_help_dic) if default else self.sub_dic
 
     ### Checkers and normalizers
     @staticmethod
@@ -200,8 +190,7 @@ class ConfigParser:
             raise ValueError(f'entity must start with "pub" or "sub" not {act}')
         return action, name
 
-
-    def parse(self, stream_or_fname=None):
+    def json_to_config(self, stream_or_fname=None):
         """parsing user-entered config data, so tolerate mixed case"""
         # Validate the keys but let the values flow to the dic consumer
         def _dict_decorate(ordered_pairs):
@@ -215,8 +204,6 @@ class ConfigParser:
                     dic[key] = value
             return dic
 
-        if not stream_or_fname:
-            return
         if isinstance(stream_or_fname, str):
             with open(stream_or_fname, 'r', encoding='utf8') as cfg_file:
                 cfg_dic = json.load(cfg_file, object_pairs_hook=_dict_decorate)
@@ -225,85 +212,96 @@ class ConfigParser:
         else:
             raise ValueError(
                 self.err_msg_('param', 'must be StringIO or str', type(stream_or_fname)))
+        return cfg_dic
+
+    def parse(self, stream_or_fname=None):
+        """parsing user-entered config data, so tolerate mixed case"""
+        if not stream_or_fname:
+            return
+        cfg_dic = self.json_to_config(stream_or_fname)
         for key in cfg_dic.keys():
             key_upper = key.upper()
             if "PUB" in key_upper:
-                self.parse_pub(cfg_dic[key], key)
+                self.normalize_pub_config(cfg_dic[key])
+                ##self.parse_pub(cfg_dic[key], key)
             elif "SUB" in key_upper:
-                self.parse_sub(cfg_dic[key], key)
+                self.parse_sub(cfg_dic)
 
-    def parse_sub(self, cfg, entity_name):
+    def normalize_pub_config(self, cfg):
+        """fail on dup attributes, synthesize required with defaults"""
+        for key in cfg.keys():
+            which = self.normalize_shape(key)
+            pd = self.parse_one_pub(which, cfg[key])
+            self.pub_list.append(pd)
+            LOG.info(f'{which=} {pd=} ')
+
+    '''def normalize_sub(self, cfg):
+        """parse the sub dictionary, saving as self.sub_list to be retrieved with get_sub_config"""
+        LOG.info('cfg:%s', cfg)
+        for key in cfg.keys():
+            which = self.normalize_shape(key)
+            self.sub_list[0][n_shape] = self.sub_default_dic
+            for attr in cfg[shape].keys():
+                value = cfg[shape][attr]
+                if self.is_content_filter(attr):
+                    self.sub_list[0][n_shape]['content_filter'] = value
+        LOG.debug('sub_list[0]: %s', self.sub_list[0])
+    '''
+    def parse_sub(self, cfg):
         """parse the sub dictionary, saving as self.sub_dic to be retrieved with get_sub_config"""
-        LOG.info('cfg:%s entity_name:%s', cfg, entity_name)
+        LOG.info('cfg:%s ', cfg)
         for shape in cfg.keys():
             n_shape = self.normalize_shape(shape)
-            self.sub_dic[n_shape] = self.sub_dic_default
+            self.sub_dic[n_shape] = self.sub_default_dic
             for attr in cfg[shape].keys():
                 value = cfg[shape][attr]
                 if self.is_content_filter(attr):
                     self.sub_dic[n_shape]['content_filter'] = value
         LOG.debug('sub_dic: %s', self.sub_dic)
 
-    def fixup_unknown(self, shape, color):
-        """helper - now that color is known, replace UNKNOWN with color"""
-        attr_lis = ['angle', 'delta_angle', 'xy', 'delta_xy', 'fillKind', 'shapesize']  # pub attrs
-        key_unknown = ConnextPublisher.form_pub_key(shape, 'UNKNOWN')
-        key_known = ConnextPublisher.form_pub_key(shape, color)
-        new_dic = {}
-        for key in self.pub_dic.keys():
-            new_dic[key] = {}
-            for attr in attr_lis:
-                if key == key_unknown:
-                    if attr == attr_lis[0]:
-                        new_dic[key_known] = {'color': color}
-                    value = self.pub_dic[key_unknown].get(attr, -1)
-                    if value != -1:
-                        new_dic[key_known][attr] = value
-                else:
-                    new_dic[key][attr] = self.pub_dic[key][attr]
-        new_dic.pop(key_unknown, None)
-        self.pub_dic = new_dic
 
-    def parse_pub(self, cfg, entity_name):
-        """parse the pub dictionary, saving as self.pub_dic to be retrieved with get_pub_config"""
-        LOG.debug(f'pub {cfg=} {cfg.keys()=} {entity_name=}')
-        #print(f'pub {cfg=} {cfg.keys()=} {entity_name=}')
-
-        if len(cfg.keys()) > 1:
-            LOG.warning("Only shape 1's config will be used.  Publishing multiple shapes is TBD")
+    def parse_one_sub(self, cfg):
+        """parse the sub dictionary, saving as self.sub_list to be retrieved with get_sub_config"""
+        LOG.info('cfg:%s', cfg)
         for shape in cfg.keys():
             n_shape = self.normalize_shape(shape)
-            key = ConnextPublisher.form_pub_key(n_shape, 'UNKNOWN')
-            self.pub_dic[key] = self.pub_dic_default
-            #LOG.info(f'{self.pub_dic_default=} \n {self.pub_dic=} \n {cfg=}')
-            color = "BLUE"
+            self.sub_list[0][n_shape] = self.sub_default_dic
             for attr in cfg[shape].keys():
-                attr_upper = attr.upper()
                 value = cfg[shape][attr]
-                if isinstance(value, str):
-                    value = value.upper()
-                if attr_upper == "COLOR":
-                    color = value
-                    self.pub_dic[key]['color'] = color
-                elif self.is_shapesize(attr_upper):
-                    self.pub_dic[key]['shapesize'] = self.normalize_int('shapesize', value)
-                elif attr_upper == "ANGLE":
-                    self.pub_dic[key]['angle'] = self.normalize_float('angle', value)
-                elif self.is_fill(attr_upper):
-                    if isinstance(value, str):
-                        value = self.normalize_fill(value)
-                    self.pub_dic[key]['fillKind'] = value
-                #elif attr_upper == "RELIABILITY_TYPE":
-                    #self.pub_dic[key]['reliability_type'] = value
-                elif attr_upper == "XY":
-                    self.pub_dic[key]['xy'] = self.normalize_xy('xy', value)
-                elif attr_upper == "DELTA_XY":
-                    self.pub_dic[key]['delta_xy'] = self.normalize_xy('delta_xy', value)
-                elif attr_upper == "DELTA_ANGLE":
-                    self.pub_dic[key]['delta_angle'] = self.normalize_float('delta_angle', value)
+                if self.is_content_filter(attr):
+                    self.sub_list[0][n_shape]['content_filter'] = value
+        LOG.debug('sub_list[0]: %s', self.sub_list[0])
 
-            self.fixup_unknown(n_shape, color)
+    def parse_one_pub(self, which, cfg):
+        """@return pub_dic by parsing a pub dictionary"""
+        LOG.info(f'pub {cfg=} {cfg.keys()=} ')
+        #print(f'pub {cfg=} {cfg.keys()=} {entity_name=}')
+        pub_dic = {which: {}}
+        for attr in cfg.keys():
+            attr_upper = attr.upper()
+            value = cfg[attr]
+            if isinstance(value, str):
+                value = value.upper()
+            if attr_upper == "COLOR":
+                color = value
+                pub_dic[which]['color'] = color
+            elif self.is_shapesize(attr_upper):
+                pub_dic[which]['shapesize'] = self.normalize_int('shapesize', value)
+            elif attr_upper == "ANGLE":
+                pub_dic[which]['angle'] = self.normalize_float('angle', value)
+            elif self.is_fill(attr_upper):
+                if isinstance(value, str):
+                    value = self.normalize_fill(value)
+                pub_dic[which]['fillKind'] = value
+            elif attr_upper == "XY":
+                pub_dic[which]['xy'] = self.normalize_xy('xy', value)
+            elif attr_upper == "DELTA_XY":
+                pub_dic[which]['delta_xy'] = self.normalize_xy('delta_xy', value)
+            elif attr_upper == "DELTA_ANGLE":
+                pub_dic[which]['delta_angle'] = self.normalize_float('delta_angle', value)
+
         #LOG.debug(f'{self.pub_dic=}')
+        return pub_dic
 
     def get_config(self, parsed_args):
         """prepare config from config or command-line"""
--- src/test_ConfigParser.py
+++ src/test_ConfigParser.py
@@ -2,8 +2,8 @@
 """tests for ConnextSubscriber"""
 
 from io import StringIO
-#import logging
-#from pprint import pprint
+import logging
+from pprint import pprint
 import unittest
 from parameterized import parameterized
 
@@ -11,8 +11,8 @@ from ConfigParser import ConfigParser
 from ShapesDemo import DEFAULT_DIC
 from ConnextPublisher import ConnextPublisher
 
-#LOG = logging.getLogger(__name__)
-#logging.basicConfig(level=logging.DEBUG)
+LOG = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 TRIANGLE_CONFIG_FILENAME = 'test_pub_t.cfg'
 
@@ -37,31 +37,42 @@ class Test(unittest.TestCase):
            }
         }}"""
         self.config_multi = """{
-        "pub1": {
           "square": {
-             "color": "color1", "xy": [27, 37],
+             "color": "green", "xy": [27, 37],
              "angle": 45, "delta_angle": 2.0,
              "delta_xy": [1,2], "size": 50
-           }
-        },
-        "pub2": {
+           },
            "square": {
-             "color": "color2", "xy": [47, 57],
+             "color": "red", "xy": [47, 57],
              "angle": 90, "delta_angle": 3.0,
              "delta_xy": [2,4], "size": 55
-           }
-           "square2": {
-             "color": "color2", "xy": [97, 97],
+           },
+           "square": {
+             "xy": [97, 97],
              "angle": 99, "delta_angle": 9.0,
              "delta_xy": [9,9], "size": 99
            }
-        }}"""
+        }"""
 
-    def test_fixup(self):
+    def test_normalize_pub_config(self):
+        config = self.parser.json_to_config(StringIO(self.config_multi))
+        self.parser.normalize_pub_config(config)
+        self.assertTrue('S' in self.parser.pub_list[0])
+        self.assertEqual('GREEN', self.parser.pub_list[0]['S']['color'], pprint(self.parser.pub_list))
+        self.assertTrue('S' in self.parser.pub_list[1])
+        self.assertEqual('RED', self.parser.pub_list[1]['S']['color'], pprint(self.parser.pub_list))
+        self.assertTrue('S' in self.parser.pub_list[2])
+        self.assertIsNone(self.parser.pub_list[2]['S'].get('color'), pprint(self.parser.pub_list))
+
+    def test_json_to_config(self):
+        cfg = self.parser.json_to_config(StringIO(self.config_multi))
+        self.assertEqual(len(cfg), 3)
+
+    def old_test_fixup(self):
         color = 'GREEN'
         key_unknown = ConnextPublisher.form_pub_key('S', 'UNKNOWN')
-        self.parser.pub_dic[key_unknown] = self.parser.pub_dic_default
-        #pprint(self.parser.pub_dic_default)
+        self.parser.pub_dic[key_unknown] = self.parser.pub_default_dic
+        #pprint(self.parser.pub_default_dic)
         self.parser.pub_dic[key_unknown]['color'] = color
         #pprint(self.parser.pub_dic)
         self.parser.fixup_unknown('S', color)
@@ -74,25 +85,25 @@ class Test(unittest.TestCase):
             self.assertIsNotNone(self.parser.pub_dic[key][attr])
 
     def test_init_does_not_populate_sub_or_pub(self):
-        self.assertFalse(self.parser.pub_dic)
+        self.assertFalse(self.parser.pub_list)
         self.assertFalse(self.parser.sub_dic)
+        ###self.assertFalse(self.parser.sub_list)
 
     def test_init_populates_defaults(self):
-        self.assertTrue(self.parser.pub_dic_default)
-        self.assertTrue(self.parser.pub_dic_help)
-        self.assertTrue(self.parser.sub_dic_default)
-        self.assertTrue(self.parser.sub_dic_help)
+        self.assertTrue(self.parser.pub_default_dic)
+        self.assertTrue(self.parser.pub_help_dic)
+        self.assertTrue(self.parser.sub_default_dic)
+        self.assertTrue(self.parser.sub_help_dic)
 
     def test_parse_sub_and_pub_no_file(self):
         self.parser.parse()
-        self.assertFalse(self.parser.sub_dic)
-        self.assertFalse(self.parser.pub_dic)
+        self.assertFalse(self.parser.get_sub_config())
+        self.assertFalse(self.parser.get_pub_config())
 
     def check_config_triangle(self):
-        key = ConnextPublisher.form_pub_key('T', "RED")
-        self.assertEqual(self.parser.pub_dic[key]['fillKind'], 3)
-        self.assertIsNone(self.parser.pub_dic.get('C'))
-        self.assertIsNone(self.parser.pub_dic.get('S'))
+        self.assertEqual(self.parser.pub_list[0]['T']['fillKind'], 3, self.parser.pub_list)
+        self.assertIsNone(self.parser.pub_list[0].get('C'))
+        self.assertIsNone(self.parser.pub_list[0].get('S'))
 
         self.assertIsNone(self.parser.sub_dic.get('C'))
         self.assertIsNone(self.parser.sub_dic.get('S'))
@@ -107,6 +118,7 @@ class Test(unittest.TestCase):
             contents = cfg_file.read()
         stream = StringIO(contents)
         self.parser.parse(stream)
+        self.check_config_triangle()
 
     def test_config_pub_square(self):
         config = """{"pub": { "square": {
@@ -118,53 +130,50 @@ class Test(unittest.TestCase):
            "size": 50
         }}}"""
         self.parser.parse(StringIO(config))
-        key = ConnextPublisher.form_pub_key('S', "YELLOW")
-        self.assertIsNotNone(self.parser.pub_dic.get(key))
-        #self.assertEqual(self.parser.pub_dic[key]['color'], "YELLOW")
-        self.assertEqual(self.parser.pub_dic[key]['xy'], [27, 37])
-        self.assertEqual(self.parser.pub_dic[key]['angle'], 45.0)
-        self.assertEqual(self.parser.pub_dic[key]['delta_angle'], 2.0)
-        self.assertEqual(self.parser.pub_dic[key]['delta_xy'], [1, 2])
-        self.assertEqual(self.parser.pub_dic[key]['shapesize'], 50)
+        self.assertEqual(len(self.parser.pub_list), 1)
+        key = 'S'
+        self.assertIsNotNone(self.parser.pub_list[0].get(key))
+        self.assertEqual(self.parser.pub_list[0][key]['color'], "YELLOW")
+        self.assertEqual(self.parser.pub_list[0][key]['xy'], [27, 37])
+        self.assertEqual(self.parser.pub_list[0][key]['angle'], 45.0)
+        self.assertEqual(self.parser.pub_list[0][key]['delta_angle'], 2.0)
+        self.assertEqual(self.parser.pub_list[0][key]['delta_xy'], [1, 2])
+        self.assertEqual(self.parser.pub_list[0][key]['shapesize'], 50)
 
     def test_minimal(self):
         config = """{"pub": { "square": {
             "size": 33
         }}}"""
         self.parser.parse(StringIO(config))
-        key = ConnextPublisher.form_pub_key('S', "BLUE")
-        self.assertIsNotNone(self.parser.pub_dic.get(key), f'{key=} {self.parser.pub_dic=}')
-        self.assertEqual(self.parser.pub_dic[key]['shapesize'], 33)
-        self.assertEqual(self.parser.pub_dic[key]['color'], 'BLUE')
-        self.assertIsNotNone(self.parser.pub_dic[key].get('xy'))
+        key = 'S'
+        self.assertIsNotNone(self.parser.pub_list[0].get(key), f'{key=} {self.parser.pub_list[0]=}')
+        self.assertEqual(self.parser.pub_list[0][key]['shapesize'], 33)
+        self.assertIsNone(self.parser.pub_list[0][key].get('color'))
+        self.assertIsNone(self.parser.pub_list[0][key].get('xy'))
 
     def test_config_pub_square2(self):
         color1, color2 = 'Yellow', 'Green'
         config = self.config.replace("color1", color1).replace("color2", color2)
         self.parser.parse(StringIO(config))
+        self.assertEqual(len(self.parser.pub_list), 2)
         #print(f'{self.parser.pub_dic=}')
-        key = ConnextPublisher.form_pub_key('S', color1.upper())
+        key = 'S'
         #print(f'{key=}')
-        self.assertIsNotNone(self.parser.pub_dic.get(key))
-        topic, color = ConnextPublisher.key_to_topic_and_color(key)
-        self.assertEqual(color, "YELLOW")
-        self.assertEqual(topic, "S")
-        self.assertEqual(self.parser.pub_dic[key]['xy'], [27, 37])
-        self.assertEqual(self.parser.pub_dic[key]['angle'], 45.0)
-        self.assertEqual(self.parser.pub_dic[key]['delta_angle'], 2.0)
-        self.assertEqual(self.parser.pub_dic[key]['delta_xy'], [1, 2])
-        self.assertEqual(self.parser.pub_dic[key]['shapesize'], 50)
+        self.assertIsNotNone(self.parser.pub_list[0].get(key))
+        self.assertEqual(self.parser.pub_list[0][key]['angle'], 45.0)
+        self.assertEqual(self.parser.pub_list[0][key]['color'], color1.upper())
+        self.assertEqual(self.parser.pub_list[0][key]['delta_angle'], 2.0)
+        self.assertEqual(self.parser.pub_list[0][key]['delta_xy'], [1, 2])
+        self.assertEqual(self.parser.pub_list[0][key]['shapesize'], 50)
+        self.assertEqual(self.parser.pub_list[0][key]['xy'], [27, 37])
 
-        key = ConnextPublisher.form_pub_key('S', color2.upper())
-        self.assertIsNotNone(self.parser.pub_dic.get(key))
-        topic, color = ConnextPublisher.key_to_topic_and_color(key)
-        self.assertEqual(color, "GREEN")
-        self.assertEqual(topic, "S")
-        self.assertEqual(self.parser.pub_dic[key]['xy'], [47, 57])
-        self.assertEqual(self.parser.pub_dic[key]['angle'], 90.0)
-        self.assertEqual(self.parser.pub_dic[key]['delta_angle'], 3.0)
-        self.assertEqual(self.parser.pub_dic[key]['delta_xy'], [2, 4])
-        self.assertEqual(self.parser.pub_dic[key]['shapesize'], 55)
+        self.assertIsNotNone(self.parser.pub_list[1].get(key))
+        self.assertEqual(self.parser.pub_list[1][key]['angle'], 90.0)
+        self.assertEqual(self.parser.pub_list[1][key]['color'], color2.upper())
+        self.assertEqual(self.parser.pub_list[1][key]['delta_angle'], 3.0)
+        self.assertEqual(self.parser.pub_list[1][key]['delta_xy'], [2, 4])
+        self.assertEqual(self.parser.pub_list[1][key]['shapesize'], 55)
+        self.assertEqual(self.parser.pub_list[1][key]['xy'], [47, 57])
 
     def test_config_pub_square_dup(self):
         color1, color2 = 'Yellow', 'Green'
@@ -172,29 +181,22 @@ class Test(unittest.TestCase):
         config = config.replace("pub1", "pub").replace("pub2", "pub")
         #pprint(config)
         self.parser.parse(StringIO(config))
-        #print(f'{self.parser.pub_dic=}')
-        key = ConnextPublisher.form_pub_key('S', color1.upper())
-        #print(f'{key=}')
-        self.assertIsNotNone(self.parser.pub_dic.get(key))
-        topic, color = ConnextPublisher.key_to_topic_and_color(key)
-        self.assertEqual(color, color1.upper())
-        self.assertEqual(topic, "S")
-        self.assertEqual(self.parser.pub_dic[key]['xy'], [27, 37])
-        self.assertEqual(self.parser.pub_dic[key]['angle'], 45.0)
-        self.assertEqual(self.parser.pub_dic[key]['delta_angle'], 2.0)
-        self.assertEqual(self.parser.pub_dic[key]['delta_xy'], [1, 2])
-        self.assertEqual(self.parser.pub_dic[key]['shapesize'], 50)
+        self.assertEqual(len(self.parser.pub_list), 2)
+        key = 'S'
+        self.assertIsNotNone(self.parser.pub_list[0].get(key))
+        self.assertEqual(self.parser.pub_list[0][key]['angle'], 45.0)
+        self.assertEqual(self.parser.pub_list[0][key]['color'], color1.upper())
+        self.assertEqual(self.parser.pub_list[0][key]['delta_angle'], 2.0)
+        self.assertEqual(self.parser.pub_list[0][key]['delta_xy'], [1, 2])
+        self.assertEqual(self.parser.pub_list[0][key]['shapesize'], 50)
+        self.assertEqual(self.parser.pub_list[0][key]['xy'], [27, 37])
 
-        key = ConnextPublisher.form_pub_key('S', color2.upper())
-        self.assertIsNotNone(self.parser.pub_dic.get(key))
-        topic, color = ConnextPublisher.key_to_topic_and_color(key)
-        self.assertEqual(color, color2.upper())
-        self.assertEqual(topic, "S")
-        self.assertEqual(self.parser.pub_dic[key]['xy'], [47, 57])
-        self.assertEqual(self.parser.pub_dic[key]['angle'], 90.0)
-        self.assertEqual(self.parser.pub_dic[key]['delta_angle'], 3.0)
-        self.assertEqual(self.parser.pub_dic[key]['delta_xy'], [2, 4])
-        self.assertEqual(self.parser.pub_dic[key]['shapesize'], 55)
+        self.assertEqual(self.parser.pub_list[1][key]['angle'], 90.0)
+        self.assertEqual(self.parser.pub_list[1][key]['color'], color2.upper())
+        self.assertEqual(self.parser.pub_list[1][key]['delta_angle'], 3.0)
+        self.assertEqual(self.parser.pub_list[1][key]['delta_xy'], [2, 4])
+        self.assertEqual(self.parser.pub_list[1][key]['shapesize'], 55)
+        self.assertEqual(self.parser.pub_list[1][key]['xy'], [47, 57])
 
     def check_case_and_length(self, default, help_text):
         #print(f'{default=} {help_text=}')
@@ -207,13 +209,19 @@ class Test(unittest.TestCase):
 
     def test_get_pub_config(self):
         self.parser.parse(TRIANGLE_CONFIG_FILENAME)
-        pub_cfg = self.parser.get_pub_config()
-        self.assertEqual(pub_cfg['T;RED']['color'], 'RED')
+        pub_cfg_list = self.parser.get_pub_config()
+        self.assertEqual(pub_cfg_list[0]['T']['color'], 'RED') #, pprint(pub_cfg_list))
 
     def test_get_sub_config(self):
         config = """{"sub": { "circle": {}, "square": {}}}"""
+        config = """{"sub": { "circle": {}}, "sub": {"square": {}}}"""
         self.parser.parse(StringIO(config))
         sub_cfg = self.parser.get_sub_config()
+        return
+        pprint(sub_cfg)
+        #print(f'{sub_cfg_list=}')
+        #self.assertEqual(len(sub_cfg_list), 1)
+        #sub_cfg = sub_cfg_list[0]
         self.assertIsNotNone(sub_cfg.get('C'))
         square_config = sub_cfg.get('S')
         self.assertIsNotNone(square_config)
