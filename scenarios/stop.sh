d4957fa94e5930d536b8e9f4b34f55afce6b350d
andy krassowski <akrassowski@gmail.com>
2023-04-07 10:41:55 -0400

    emit newline after prompt

--- scenarios/stop.sh
+++ scenarios/stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 #
 
-read -p "Hit any key to kill all Python processes" -n 1 yn
+read -p $'Hit any key to kill all Python processes\n' -n 1 yn
 
 if [ "$OS" == "Windows_NT" ]; then
   taskkill -F -IM python*
